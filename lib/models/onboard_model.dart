6a1f131047a1d32349c260aee2b61859a2b13b92
aoffahad-admin <omar.faruque@nexdecade.com>
2024-09-12 10:45:32 +0600

    Initial commit

---
+++ lib/models/onboard_model.dart
@@ -0,0 +1,41 @@
+class OnBoard {
+  final String image, text1, text2;
+  OnBoard({
+    required this.image,
+    required this.text1,
+    required this.text2,
+  });
+}
+
+List<OnBoard> onboards = [
+  OnBoard(
+      image:
+          "https://images.unsplash.com/photo-1500375592092-40eb2168fd21?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJhbHxlbnwwfHwwfHx8MA%3D%3D",
+      text1: 'Enjoy your',
+      text2: 'trip in nature'),
+  OnBoard(
+      image:
+          "https://images.unsplash.com/photo-1609137144813-7d9921338f24?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+      text1: 'Start your',
+      text2: 'journey in coding'),
+  OnBoard(
+      image:
+          "https://images.unsplash.com/photo-1543746746-46047c4f4bb0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D",
+      text1: 'Let\'s make',
+      text2: 'your dream vacation'),
+  // OnBoard(
+  //     image:
+  //         "https://images.unsplash.com/photo-1523345863760-5b7f3472d14f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8fA%3D%3D",
+  //     text1: 'Enjoy your',
+  //     text2: 'trip in nature'),
+  // OnBoard(
+  //     image:
+  //         "https://images.unsplash.com/photo-1534537841395-2e594ba9ed4a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MjV8fHxlbnwwfHx8fHw%3D",
+  //     text1: 'Enjoy your',
+  //     text2: 'trip in nature'),
+  // OnBoard(
+  //     image:
+  //         "https://images.unsplash.com/photo-1496158866946-4c08bf8159af?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mzl8fHxlbnwwfHx8fHw%3D",
+  //     text1: 'Enjoy your',
+  //     text2: 'trip in nature'),
+];
